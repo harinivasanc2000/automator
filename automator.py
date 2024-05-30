f1a985d7dc58f0a3817ad56332c433e6a2429087
Harinivasan Chandrasekaran <2823328C@student.gla.ac.uk>
2024-05-30 11:56:22 +0100

    change

--- automator.py
+++ automator.py
@@ -157,6 +157,85 @@ print(compressed_class())
 print(application_class())
 
 
+import os
+import shutil
+
+source_folder = '/Users/harinivasanchandrasekaran/Downloads'
+
+
+def get_unique_filename(filename, destination_folder, extension):
+    
+
+    base_filename, _ = os.path.splitext(filename)  # Separate filename and extension
+    counter = 1
+    while os.path.exists(os.path.join(destination_folder, f"{base_filename} ({counter}){extension}")):
+        counter += 1
+    return os.path.join(destination_folder, f"{base_filename} ({counter}){extension}")
+
+
+def handle_file_move(file_type, source_path, destination_folder, extensions):
+    
+
+    try:
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        file_list = [
+            pos_file for pos_file in os.listdir(source_folder) if pos_file.lower().endswith(extensions)
+        ]
+
+        no_files = 0
+        for file in file_list:
+            source_file_path = os.path.join(source_folder, file)
+            destination_file_path = get_unique_filename(file, destination_folder, os.path.splitext(file)[1])
+            shutil.move(source_file_path, destination_file_path)
+            no_files += 1
+
+        print(f"Total {file_type.capitalize()} Files Automated: {no_files}")
+
+    except OSError as e:
+        print(f"There were some issues when creating directories or moving files: {e}")
+
+
+def image_class():
+    image_folder = os.path.join(source_folder, 'Automated/Images')
+    image_extensions = ('.jpeg', '.jpg', '.heic', '.png', '.bmp', '.tif', '.tiff', '.svg', '.webp', '.heif', '.psd', '.raw', '.gif')
+    handle_file_move('image', source_folder, image_folder, image_extensions)
+
+
+def document_class():
+    document_folder = os.path.join(source_folder, 'Automated/Documents')
+    document_extensions = ('.pdf', '.txt', '.word', '.rtf', '.docx', '.doc', '.xlsx', '.pptx', '.pages', '.numbers', '.key', '.csv', '.ini', '.iso', '.xml', '.html', '.json', '.cfg', 'epub', 'exml', 'ics', '.md')
+    handle_file_move('document', source_folder, document_folder, document_extensions)
+    
+
+def code_class():
+    document_folder = os.path.join(source_folder, 'Automated/Code')
+    document_extensions = ('.pdf', '.txt', '.word', '.rtf', '.docx', '.doc', '.xlsx', '.pptx', '.pages', '.numbers', '.key', '.csv', '.ini', '.iso', '.xml', '.html', '.json', '.cfg', 'epub', 'exml', 'ics', '.md')
+    handle_file_move('document', source_folder, document_folder, document_extensions)
+    
+def compressed_class():
+    document_folder = os.path.join(source_folder, 'Automated/Compressed')
+    document_extensions = ('.pdf', '.txt', '.word', '.rtf', '.docx', '.doc', '.xlsx', '.pptx', '.pages', '.numbers', '.key', '.csv', '.ini', '.iso', '.xml', '.html', '.json', '.cfg', 'epub', 'exml', 'ics', '.md')
+    handle_file_move('document', source_folder, document_folder, document_extensions)
+    
+def application_class():
+    document_folder = os.path.join(source_folder, 'Automated/Application')
+    document_extensions = ('.pdf', '.txt', '.word', '.rtf', '.docx', '.doc', '.xlsx', '.pptx', '.pages', '.numbers', '.key', '.csv', '.ini', '.iso', '.xml', '.html', '.json', '.cfg', 'epub', 'exml', 'ics', '.md')
+    handle_file_move('document', source_folder, document_folder, document_extensions)
+    
+
+
+
+
+print(image_class())
+print(video_class())
+print(document_class())
+print(code_class())
+print(compressed_class())
+print(application_class())
+
+
 
 
 
